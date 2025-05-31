4e920deefdef06611e108fc2a9050e32a95ae133
Eric599 <cuenat.eric@gmail.com>
2025-05-31 15:17:29 -0300

    conditional component rendering for new task component

---
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -0,0 +1,60 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { TaskComponent } from './task/task.component'
+import { NewTaskComponent } from './new-task/new-task.component'
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  isNewTask = false
+  @Input({ required: true }) userId!: string
+  @Input({ required: true }) name?: string
+  @Output() newTask = new EventEmitter<string>()
+
+  dummyTasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ]
+
+  get selectedUserTasks() {
+    return this.dummyTasks.filter((task) => task.userId == this.userId)
+  }
+
+  onCompletedTask(id: string) {
+    this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id)
+  }
+
+  onCreateTask() {
+    this.isNewTask = true
+    console.log('New task for ' + this.name)
+  }
+
+  onCanceledAddTask() {
+    this.isNewTask = false
+  }
+}
--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,23 +1,52 @@
-import { Component, Input } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { User } from './user.model'
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-    // input decorator is similar to setting props in React
-    // if we are going to use '!' to assert, always set required to true
-    @Input({required: true}) avatar!: string
-    @Input({required: true}) name!: string
-
-    get imagePath() {
-        return 'assets/users/' + this.avatar
-    }
-    onSelectUser() { }
+  // input decorator is similar to setting props in React
+  // if we are going to use '!' to assert, always set required to true
+  // @Input({ required: true }) id!: string
+  // @Input({ required: true }) avatar!: string
+  // @Input({ required: true }) name!: string
+  @Input({ required: true }) user!: User
+  @Input({ required: true }) isSelected!: boolean
+  @Output() selected = new EventEmitter<User>()
+  // using output function (not a signal like input function!!)
+  // actually does the exact same thing as @Output, just a new way to do things
+  // select = output<string>()
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar
+  }
+  onSelectUser() {
+    this.selected.emit(this.user)
+  }
 }
+
+// example using signal input, import lowercase input instead, new way of doing things
+// import { Component, input, computed } from '@angular/core';
+
+// @Component({
+//   selector: 'app-user',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './user.component.html',
+//   styleUrl: './user.component.css'
+// })
+
+// export class UserComponent {
+//     avatar = input.required<string>()
+//     name = input.required<string>()
+
+//     imagePath = computed(() => {
+//         return 'assets/users/' + this.avatar()
+//     })
+//     onSelectUser() {
+
+//     }
+// }
