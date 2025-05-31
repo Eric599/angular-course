ab5e4be2a19f3ec727105b67f892e2e623634b19
Eric599 <cuenat.eric@gmail.com>
2025-05-31 16:15:57 -0300

    2 way binding with forms

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,17 +1,32 @@
 import { Component, EventEmitter, Output } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { NewTaskData } from '../task/task.model'
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() cancelTask = new EventEmitter<void>()
+  @Output() newTask = new EventEmitter<NewTaskData>()
+  // enteredTitle = signal('') can also use signal like this, html does not need to change
+  enteredTitle = ''
+  enteredDate = ''
+  enteredSummary = ''
 
   onCancelTask() {
     console.log('task canceled')
     this.cancelTask.emit()
+  }
+
+  onSubmit() {
+    this.newTask.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      dueDate: this.enteredDate,
+    })
   }
 }
--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { TaskComponent } from './task/task.component'
 import { NewTaskComponent } from './new-task/new-task.component'
+import { NewTaskData } from './task/task.model'
 
 @Component({
   selector: 'app-tasks',
@@ -55,6 +56,17 @@ export class TasksComponent {
   }
 
   onCanceledAddTask() {
+    this.isNewTask = false
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.dummyTasks.push({
+      id: new Date().getDate().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate,
+    })
     this.isNewTask = false
   }
 }
